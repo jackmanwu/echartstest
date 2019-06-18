9c821e0ba44c268706674bfc4818cb3da3e85d41
wujinlei <wujinlei@cd.tuan800.com>
2019-06-18 18:26:27 +0800

    echarts练习

---
+++ src/main/java/com/jackmanwu/Server.java
@@ -0,0 +1,14 @@
+package com.jackmanwu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by JackManWu on 2019/6/17.
+ */
+@SpringBootApplication
+public class Server {
+    public static void main(String[] args) {
+        SpringApplication.run(Server.class);
+    }
+}
